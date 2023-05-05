c6e9f88615304a356146ee3ea36450f86885cf7c
LeergutLars <119765991+LeergutLarss@users.noreply.github.com>
2023-05-05 13:40:36 +0200

    Basic files + basic math operation js funcitons

---
+++ script.js
@@ -0,0 +1,16 @@
+function add(operant, operant2) {
+    return operant + operant2;
+}
+
+function sub(operant, operant2) {
+    return operant - operant2;
+}
+
+function mul(operant, operant2) {
+    return operant * operant2;
+}
+
+function div(operant, operant2) {
+    return operant / operant2;
+}
+
