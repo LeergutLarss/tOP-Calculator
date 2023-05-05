4ea52a9e6c872b0be287be8c992a97c6a65fc29d
LeergutLars <119765991+LeergutLarss@users.noreply.github.com>
2023-05-05 13:51:37 +0200

    Add calc Funktion

--- script.js
+++ script.js
@@ -19,3 +19,24 @@ function mul(operant, operant2) {
 function div(operant, operant2) {
     return operant / operant2;
 }
+
+function calculation(operant, operator, operant2) {
+    let result = 0;
+    switch(operant) {
+        case('+'):
+            result = add(operant, operant2);
+            break;
+        case('-'):
+            result = sub(operant, operant2);
+            break;
+        case('*'):
+            result = mul(operant, operant2);
+            break;
+        case('/'):
+            result = div(operant, operant2);
+            break;
+        default:
+            return 'ERR: Calculation';
+    }
+    return result;
+}
