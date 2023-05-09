ba475cf386890b828f80ea31be38080bfb367a43
LeergutLars <119765991+LeergutLarss@users.noreply.github.com>
2023-05-09 10:45:23 +0200

    Clear button and full functional input

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 //input
 const input = {
     firstValue: "",
-    secondValue: "",
     operator: "",
+    secondValue: "",
     isFirst: true,
     start: true,
     comma: false,
 }
 const numbers = Array.from(document.querySelectorAll('.numbers button'));
+const operators = Array.from(document.querySelectorAll('.operations button'));
+const clr = document.getElementById('clr');
 //Events
 numbers.forEach((number) => {
     number.addEventListener('click', () => {
-        if(!(number.textContent == '.' && input.start) || !(number.textContent == '.' && input.comma)) {
+        if(!(number.textContent == '.' && input.start) && !(number.textContent == '.' && input.comma)) {
             if(number.textContent == '.') {
                 input.comma = true;
             }
@@ -19,7 +21,28 @@ numbers.forEach((number) => {
         }
     });
 });
-
+operators.forEach((operat) => {
+    operat.addEventListener('click', () => {
+        if(operat.textContent == '=') {
+            // CALC LATORER TODO
+        } else {
+            if(input.operator == "") {
+                input.isFirst = false;
+                input.start = true;
+                input.comma = false;
+                input.operator = operat.textContent;                
+            }
+        }
+    })
+})
+clr.addEventListener('click', () => {
+    input.firstValue = "";
+    input.secondValue = ""; 
+    input.operator = "";
+    input.isFirst = true;
+    input.start = true;
+    input.comma = false;
+})
 
 function numberInput(number, input) {
         if(input.isFirst) {
@@ -27,5 +50,6 @@ function numberInput(number, input) {
         } else {
             input.secondValue += number;
         } 
+        input.start = false;
     console.log(input);
 }
